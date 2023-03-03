50c02ebce93e4dc39b5c354a093adcd1f40aceec
shengjiehou123 <945947485@qq.com>
2023-03-03 19:03:35 +0800

    add like me page

--- BlindDateApp/LikeMePage/LikeMe.swift
+++ BlindDateApp/LikeMePage/LikeMe.swift
@@ -10,12 +10,15 @@ import SDWebImageSwiftUI
 
 class LikeMeViewModel:ObservableObject{
     @Published var listData : [LikeMeModel] = []
-    init(){
-        requestLikeMeList(state: .normal)
-    }
+    @Published var total : Int = 0
     func requestLikeMeList(state:RefreshState){
         let param = ["page":1,"pageLimit":10]
         NW.request(urlStr: "like/me/list", method: .post, parameters: param) { response in
+            if state == .normal || state == .pullDown || state == .refresh {
+                self.listData.removeAll()
+            }
+            let total = response.data["total"] as? Int ?? 0
+            self.total = total
             guard let list = response.data["likeMeList"] as? [[String:Any]] else{
                 return
             }
@@ -46,16 +49,37 @@ class LikeMeViewModel:ObservableObject{
 struct LikeMe: View {
     @ObservedObject var model : LikeMeViewModel = LikeMeViewModel()
     var body: some View {
-        ScrollView(.vertical, showsIndicators: false) {
-            let items = [GridItem(.flexible()),GridItem(.flexible())]
-            LazyVGrid(columns: items,spacing: 10) {
-                ForEach(model.listData,id:\.id){ model in
-                    WebImage(url: URL(string: model.avatar)).resizable().aspectRatio( contentMode: .fill).frame(height:250).clipShape(RoundedRectangle(cornerRadius: 10))
+        NavigationView{
+            ScrollView(.vertical, showsIndicators: false) {
+                if model.total > 0 {
+                    VStack(alignment: .center, spacing: 10){
+                        Spacer().frame(height:10)
+                        Text("开通特权，立即解锁喜欢你的人")
+                            .font(.system(size: 17, weight: .medium, design: .default))
+                        Text("有\(model.total)人正在等待你的回应").font(.system(size: 17, weight: .medium, design: .default))
+                    }
                 }
-            }.padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
-        }.onAppear {
-          
+                
+                let items = [GridItem(.flexible()),GridItem(.flexible())]
+                LazyVGrid(columns: items,spacing: 10) {
+                    ForEach(model.listData,id:\.id){ model in
+                        WebImage(url: URL(string: model.avatar)).resizable().aspectRatio( contentMode: .fill).frame(height:250).clipShape(RoundedRectangle(cornerRadius: 10))
+                    }
+                }.padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
+            }.navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        if model.total > 0 {
+                            Text("\(model.total)人喜欢我")
+                                .font(.system(size: 25, weight: .medium, design: .default))
+                        }
+                    }
+                }.onAppear {
+                    model.requestLikeMeList(state: .normal)
+                }
         }
+          
+        
     }
 }
 
