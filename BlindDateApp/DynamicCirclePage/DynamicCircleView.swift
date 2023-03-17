9364c0e9e834eac60a57cb439138ea3267dea553
shengjiehou123 <945947485@qq.com>
2023-03-17 15:57:13 +0800

    add comment list

--- BlindDateApp/DynamicCirclePage/DynamicCircleView.swift
+++ BlindDateApp/DynamicCirclePage/DynamicCircleView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 import SDWebImageSwiftUI
 import HandyJSON
 import JFHeroBrowser
+import Combine
 
-class CircleModel:HandyJSON{
+class CircleModel:HandyJSON,ObservableObject{
+    var id : Int = 0
     var uid : Int = 0
     var content : String = ""
     var images : String = ""
@@ -72,7 +74,8 @@ struct DynamicCircleView: View {
 }
 
 struct CircleRow:View{
-    var model : CircleModel
+    @StateObject var model : CircleModel
+    @State var showComment : Bool = false
     @State var images: [String] = []
     var body: some View{
         VStack(alignment: .leading, spacing: 15) {
@@ -120,11 +123,15 @@ struct CircleRow:View{
             HStack(alignment:.center,spacing:50){
                 Spacer()
                 Text("点赞")
-                Text("评论")
+                Text("评论").onTapGesture {
+                    showComment = true
+                }
             }.padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 20))
             
         }.onAppear {
             images = model.images.components(separatedBy: ",")
+        }.alertB(isPresented: $showComment) {
+            CommentListView(show:$showComment,circleId: $model.id)
         }
     }
     
@@ -138,12 +145,284 @@ struct CircleRow:View{
     }
 }
 
+class ObserVedCommentModel:ObservableObject,Identifiable{
+    @Published var titles : [CommentModel] = []
+}
 
+struct CommentListView:View{
+    @Binding var show : Bool
+    @Binding var circleId : Int
+//    @StateObject var obModel : ObserVedCommentModel = ObserVedCommentModel()
+    @State var titles : [CommentModel] = []
+    @State var showSecondaryList : Bool = false
+    @State var showAnimation : Bool = false
+    @State var comment : String = ""
+    var body: some View{
+        if show{
+    ZStack(alignment: .bottomLeading){
+        ZStack(alignment: .bottomLeading) {
+            Rectangle().fill(Color.black.opacity(0.3))
+        VStack(alignment: .leading, spacing: 0){
+            HStack(alignment: .center, spacing: 0){
+                Text(titles.count > 0 ? "评论\(titles.count)" : "评论").font(.system(size: 18, weight: .medium, design: .default))
+                Spacer()
+                Button {
+                    showAnimation = false
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
+                        self.topViewController()?.dismiss(animated: false, completion: nil)
+                        show = false
+                    }
+                } label: {
+                    HStack{
+                        Image("close").resizable().renderingMode(.original).aspectRatio( contentMode: .fill).frame(width: 24, height: 24, alignment: .center)
+                    }.padding(EdgeInsets(top: 8, leading: 20, bottom: 8, trailing: 20))
+                }
 
+                
+            }.padding(EdgeInsets(top: 0, leading: 25, bottom: 0, trailing: 0)).frame(height:50)
+        ScrollView(.vertical,showsIndicators: false){
+            ScrollViewReader { reader in
+                LazyVStack(alignment: .leading, spacing: 10){
+                
+                ForEach(titles,id:\.id){ model in
+                    Section(header: CommentSection(model:model)) {
+                        SecondaryRowList(model: model)
+                    }
+                    
+                }
+                    
+                }.onChange(of: titles) { _ in
+                 reader.scrollTo(titles[0].id, anchor: .center)
+             }
+            }
+                
+        }.padding(.bottom,65 + kSafeBottom)
+        
+           
+        }.background((RoundedCorner(corners: [.topLeft,.topRight], radius: 10).fill(Color.white))).frame(maxWidth:.infinity,maxHeight: 600).offset(y:showAnimation ? 0: 620).animation(.linear(duration: 0.25), value: showAnimation).onAppear {
+            showAnimation = true
+         }
+            
+        }.edgesIgnoringSafeArea(.all).onAppear {
+            requestCommentList(state: .normal)
+       }
+            
+        CommentSendMsgView(circleId: $circleId,sendCommentSucHandle: {
+            requestCommentList(state: .normal)
+        })
+    }.ignoresSafeArea(edges: .bottom)
+     }
+    }
+    func requestCommentList(state:RefreshState){
+        let params = ["circleId":circleId,"page":1,"pageLimit":10]
+        NW.request(urlStr: "comment/list",method: .post,parameters: params) { response in
+            guard let list = response.data["list"] as? [[String:Any]] else{
+                return
+            }
+            if state == .normal || state == .pullDown {
+                titles.removeAll()
+            }
+            for item in list {
+                guard let model = CommentModel.deserialize(from: item, designatedPath: nil) else{
+                    continue
+                }
+                titles.append(model)
+            }
+        } failedHandler: { response in
+            
+        }
+    }
+    
+    
+    
+    
+}
 
+//MARK: 发送评论
+struct CommentSendMsgView:View{
+    @Binding var circleId : Int
+    @State var comment : String = ""
+    var sendCommentSucHandle : () ->Void
+    @State var keyBoardShow : Bool = false
+    var body: some View{
+        HStack(alignment: .center, spacing: 0) {
+            TextField("评论...", text: $comment,onCommit: {
+                if !comment.isEmpty {
+                    requestCreateComment()
+                }
+            }).frame(maxWidth:.infinity,maxHeight:50).padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20)).background(Capsule().fill(Color.colorWithHexString(hex: "#F3F3F3"))).onChange(of: comment) { newValue in
+                comment = comment.trimmingCharacters(in: .whitespaces)
+            }.onReceive(Publishers.keyboardHeight) {
+                let keyboardHeight = $0
+                keyBoardShow  = keyboardHeight > 0 ? true : false
+            }
+        }.padding(EdgeInsets(top: 10, leading: 20, bottom: keyBoardShow ? 5 : kSafeBottom, trailing: 20)).introspectTextField { textfield in
+            textfield.returnKeyType = .send
+        }.background(Color.white).keyboardAdaptive()
+    }
+    func requestCreateComment(){
+        let params = ["circleId":circleId,"comment":comment] as [String : Any]
+        NW.request(urlStr: "create/comment", method: .post, parameters: params) { response in
+            comment = ""
+            sendCommentSucHandle()
+        } failedHandler: { response in
+            
+        }
 
-struct DynamicCircle_Previews: PreviewProvider {
-    static var previews: some View {
-        DynamicCircleView()
     }
 }
+
+struct SecondaryRowList:View{
+    @State var page : Int = 1
+    @StateObject var model : CommentModel
+    @State var show:Bool = true
+    var body: some View{
+        
+        if show {
+            ForEach(model.list,id:\.id){ secondaryCommentModel in
+                SecondaryCommentRow(model: secondaryCommentModel)
+            }
+        }
+        
+        if model.secondaryCount > 0{
+            if model.list.count < model.secondaryCount || !show{
+                HStack(alignment: .center, spacing: 0) {
+                    Spacer().frame(width:60)
+                    Text("–– 展开\(!show ? model.list.count : model.secondaryCount-model.list.count)条回复").font(.system(size: 13, weight: .medium, design: .default))
+                    Image("pull_down_indicator").resizable().renderingMode(.template).foregroundColor(.black).aspectRatio(contentMode: .fill)
+                        .frame(width: 14, height: 14, alignment: .center)
+                }.onTapGesture {
+                    if(model.secondaryCount > 0 && model.secondaryCount == model.list.count){
+                        show = true
+                        return
+                    }
+                    requestSecondaryCommentList(model: model,state: .pullUp)
+                }
+            }else{
+                HStack(alignment: .center, spacing: 0) {
+                    Spacer().frame(width:60)
+                    Text("–– 收起").font(.system(size: 13, weight: .medium, design: .default))
+                    Image("pull_down_indicator").resizable().renderingMode(.template).foregroundColor(.black).aspectRatio(contentMode: .fill)
+                        .frame(width: 14, height: 14, alignment: .center)
+                }.onTapGesture {
+                    show = false
+                }
+            }
+        }
+    }
+    
+    func requestSecondaryCommentList(model:CommentModel,state:RefreshState){
+        if state != .pullUp {
+            self.page = 1
+        }
+        let params = ["commentId":model.id,"page":self.page,"pageLimit":1]
+        NW.request(urlStr: "secondary/comment/list", method: .post, parameters: params) { response in
+            guard let list = response.data["list"] as? [[String:Any]] else{
+                return
+            }
+            self.page += 1
+            for item in list {
+                guard let secondaryCommentModel = SecondaryCommentModel.deserialize(from: item, designatedPath: nil) else{
+                    continue
+                }
+                model.list.append(secondaryCommentModel)
+            }
+        } failedHandler: { response in
+            
+        }
+
+    }
+}
+
+struct CommentSection:View{
+    var model : CommentModel
+    var body: some View{
+        VStack(alignment: .leading, spacing: 5) {
+            HStack(alignment: .top, spacing: 5){
+                Spacer().frame(width:20)
+                WebImage(url: URL(string: model.userInfo.avatar)).resizable().aspectRatio(contentMode: .fill).background(Color.gray).clipShape(Circle()).frame(width: 30, height: 30, alignment: .center)
+                VStack(alignment: .leading, spacing: 3){
+                    Text(model.userInfo.nickName).font(.system(size: 13)).foregroundColor(.colorWithHexString(hex: "#999999"))
+                    Text(model.comment).font(.system(size: 15)).lineSpacing(5)
+                }
+                Spacer()
+            }
+                HStack(alignment: .center, spacing: 5){
+                    Spacer().frame(width:55)
+                    Text(model.createAt).font(.system(size: 12)).foregroundColor(.colorWithHexString(hex: "#999999"))
+                    Text("回复").font(.system(size: 12, weight: .medium, design: .default)).foregroundColor(.colorWithHexString(hex: "#999999"))
+                }
+        }
+    }
+}
+
+struct SecondaryCommentRow:View{
+    var model : SecondaryCommentModel
+    var body: some View{
+        VStack(alignment: .leading, spacing: 0) {
+            HStack(alignment: .center, spacing: 5){
+                Spacer().frame(width:55)
+                WebImage(url: URL(string: model.uidInfo.avatar)).resizable().aspectRatio(contentMode: .fill).background(Color.gray).clipShape(Circle()).frame(width: 20, height: 20, alignment: .center)
+                Text(model.atUid > 0 ? "\(model.uidInfo.nickName)►\(model.atUidInfo.nickName)" : "\(model.uidInfo.nickName)").font(.system(size: 13)).foregroundColor(.colorWithHexString(hex: "#999999"))
+                Spacer()
+            }
+            Spacer().frame(height:3)
+            HStack(alignment: .center, spacing: 0) {
+                Spacer().frame(width:85)
+                Text(model.comment).font(.system(size: 15)).lineSpacing(5)
+            }
+            Spacer().frame(height:5)
+            HStack(alignment: .center, spacing: 5){
+                Spacer().frame(width:80)
+                Text(model.createAt).font(.system(size: 12)).foregroundColor(.gray)
+                Text("回复").font(.system(size: 12, weight: .medium, design: .default)).foregroundColor(.colorWithHexString(hex: "#999999"))
+            }
+        }
+    }
+}
+
+class CommentModel:HandyJSON,ObservableObject,Identifiable,Equatable{
+    static func == (lhs: CommentModel, rhs: CommentModel) -> Bool {
+        return lhs.id == rhs.id
+    }
+    
+    var id : Int = 0
+    var circleId : Int = 0
+    var uid : Int = 0
+    var comment: String = ""
+    var createAt : String = ""
+    var secondaryCount :Int = 0
+    var userInfo : CircleUserInfo = CircleUserInfo()
+    @Published var list : [SecondaryCommentModel] = []
+    required init() {
+        
+    }
+}
+
+class SecondaryCommentModel:HandyJSON,Identifiable{
+    var id :Int = 0
+    var commentId :Int = 0
+    var uid : Int = 0
+    var atUid : Int = 0
+    var comment : String = ""
+    var createAt : String = ""
+    var updateAt : String = ""
+    var uidInfo:CircleUserInfo = CircleUserInfo()
+    var atUidInfo:CircleUserInfo = CircleUserInfo()
+    required init() {
+        
+    }
+}
+
+//struct CommentListView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        CommentListView(show: .constant(true),circleId: 1)
+//
+//    }
+//}
+
+//struct DynamicCircle_Previews: PreviewProvider {
+//    static var previews: some View {
+//        DynamicCircleView()
+//    }
+//}
--- BlindDateApp/Utils/ViewExtension.swift
+++ BlindDateApp/Utils/ViewExtension.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ViewControllerHolder {
     weak var value: UIViewController?
@@ -41,6 +42,10 @@ extension View{
        }
     func hidenKeyBoard(){
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+    
+    func keyboardAdaptive() -> some View {
+        ModifiedContent(content: self, modifier: KeyboardAdaptive())
     }
     
     func alertB<Content:View>(isPresented: Binding<Bool>, @ViewBuilder builder: () -> Content) -> some View {
@@ -160,6 +165,43 @@ struct Toast:View{
             
         }
         
+    }
+}
+
+extension Publishers {
+    // 1.
+    static var keyboardHeight: AnyPublisher<CGFloat, Never> {
+        // 2.
+        let willShow = NotificationCenter.default.publisher(for: UIApplication.keyboardWillShowNotification)
+            .map {
+                $0.keyboardHeight
+                
+            }
+        
+        let willHide = NotificationCenter.default.publisher(for: UIApplication.keyboardWillHideNotification)
+            .map { _ in CGFloat(0) }
+        
+        // 3.
+        return MergeMany(willShow, willHide)
+            .eraseToAnyPublisher()
+    }
+}
+
+extension Notification {
+    var keyboardHeight: CGFloat {
+        return (userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect)?.height ?? 0
+    }
+}
+
+struct KeyboardAdaptive: ViewModifier {
+    @State private var keyboardHeight: CGFloat = 0
+
+    func body(content: Content) -> some View {
+        content
+            .padding(.bottom, keyboardHeight)
+            .onReceive(Publishers.keyboardHeight) { self.keyboardHeight = $0
+                
+            }
     }
 }
 
