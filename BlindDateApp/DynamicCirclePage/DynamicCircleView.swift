fa23a7da8cad92833a5b463a052840b9aa440cc1
shengjiehou123 <945947485@qq.com>
2023-03-19 22:01:17 +0800

    fix detail

--- BlindDateApp/DynamicCirclePage/DynamicCircleView.swift
+++ BlindDateApp/DynamicCirclePage/DynamicCircleView.swift
@@ -191,8 +191,8 @@ struct CommentListView:View{
                 
                 ForEach(titles,id:\.id){ model in
                     Section(header: CommentSection(model:model).environmentObject(observerTapModel)) {
-                        SecondaryRowList(model: model){
-                            reader.scrollTo(model.list.last?.id, anchor: .center)
+                        SecondaryRowList(model: model) { show in
+                            show ? reader.scrollTo(model.list.last?.id, anchor: .center) : reader.scrollTo(model.id, anchor: .center)
                         }.environmentObject(observerTapModel)
                         
                     }
@@ -313,18 +313,19 @@ struct CommentSendMsgView:View{
 }
 
 struct SecondaryRowList:View{
+    @State var show:Bool = true
     @State var page : Int = 1
     @StateObject var model : CommentModel
-    @State var show:Bool = true
-    var listChangeHandle : () ->Void
+    var listChangeHandle : (_ show:Bool) ->Void
     @EnvironmentObject var tapModel : ObserverTapModel
+    private let pageLimit = 10
     var body: some View{
         
         if show {
             ForEach(model.list,id:\.id){ secondaryCommentModel in
-                SecondaryCommentRow(model: secondaryCommentModel)
+                SecondaryCommentRow(model: secondaryCommentModel).environmentObject(tapModel).id(secondaryCommentModel.id)
             }.onChange(of: model.list) { _ in
-                listChangeHandle()
+                listChangeHandle(show)
             }.onReceive(tapModel.$sendMsgSuc) { newValue in
                 log.info("model.id\(model.id) tapModel.commentId\(tapModel.commentId) tapModel.sendMsgSuc \(newValue)")
                 if model.id == tapModel.commentId && newValue{
@@ -336,36 +337,50 @@ struct SecondaryRowList:View{
         
         if model.secondaryCount > 0 || model.list.count > 0{
             if model.list.count < model.secondaryCount || !show{
-                HStack(alignment: .center, spacing: 0) {
-                    Spacer().frame(width:60)
-                    Text("–– 展开\(!show ? model.list.count : model.secondaryCount-model.list.count)条回复").font(.system(size: 13, weight: .medium, design: .default))
-                    Image("pull_down_indicator").resizable().renderingMode(.template).foregroundColor(.black).aspectRatio(contentMode: .fill)
-                        .frame(width: 14, height: 14, alignment: .center)
-                }.onTapGesture {
+                Button {
                     if(model.secondaryCount > 0 && model.secondaryCount == model.list.count){
                         show = true
                         return
                     }
                     requestSecondaryCommentList(commentId: model.id, state: .pullUp)
-                }
+                } label: {
+                    HStack(alignment: .center, spacing: 0) {
+                        Spacer().frame(width:60)
+                        Text("–– 展开\(!show ? model.list.count : model.secondaryCount-model.list.count)条回复").font(.system(size: 13, weight: .medium, design: .default))
+                        Image("pull_down_indicator").resizable().renderingMode(.template).foregroundColor(.black).aspectRatio(contentMode: .fill)
+                            .frame(width: 14, height: 14, alignment: .center)
+                        Spacer().frame(width:45)
+                    }
+                }.buttonStyle(PlainButtonStyle()).foregroundColor(.black)
+
+               
             }else{
-                HStack(alignment: .center, spacing: 0) {
-                    Spacer().frame(width:60)
-                    Text("–– 收起").font(.system(size: 13, weight: .medium, design: .default))
-                    Image("pull_down_indicator").resizable().renderingMode(.template).foregroundColor(.black).aspectRatio(contentMode: .fill)
-                        .frame(width: 14, height: 14, alignment: .center)
-                }.onTapGesture {
+                
+                Button {
                     show = false
-                }
+                    listChangeHandle(show)
+                } label: {
+                    HStack(alignment: .center, spacing: 0) {
+                        Spacer().frame(width:60)
+                        Text("–– 收起").font(.system(size: 13, weight: .medium, design: .default))
+                        Image("pull_down_indicator").resizable().renderingMode(.template).foregroundColor(.black).aspectRatio(contentMode: .fill)
+                            .frame(width: 14, height: 14, alignment: .center)
+                    }
+                }.buttonStyle(PlainButtonStyle()).foregroundColor(.black)
+
+               
             }
         }
     }
     
     func requestSecondaryCommentList(commentId:Int,state:RefreshState){
-        if state != .pullUp {
-            self.page = 1
+        var page = 1
+        if model.list.count > 0 {
+            page =  model.list.count / pageLimit + 1
         }
-        let params = ["commentId":commentId,"page":self.page,"pageLimit":10]
+        self.page = page
+       
+        let params = ["commentId":commentId,"page":self.page,"pageLimit":pageLimit]
         NW.request(urlStr: "secondary/comment/list", method: .post, parameters: params) { response in
             guard let list = response.data["list"] as? [[String:Any]] else{
                 return
@@ -375,14 +390,14 @@ struct SecondaryRowList:View{
             }
             model.secondaryCount = totalCount
 //            self.page += 1
-            if state != .pullUp {
-                model.list.removeAll()
-            }
             for item in list {
                 guard let secondaryCommentModel = SecondaryCommentModel.deserialize(from: item, designatedPath: nil) else{
                     continue
                 }
-                model.list.append(secondaryCommentModel)
+                if !model.list.contains(where: { $0.id == secondaryCommentModel.id}) {
+                    model.list.append(secondaryCommentModel)
+                }
+               
             }
         } failedHandler: { response in
             
@@ -395,53 +410,72 @@ struct CommentSection:View{
     var model : CommentModel
     @EnvironmentObject var tapModel : ObserverTapModel
     var body: some View{
-        VStack(alignment: .leading, spacing: 5) {
-            HStack(alignment: .top, spacing: 5){
-                Spacer().frame(width:20)
-                WebImage(url: URL(string: model.userInfo.avatar)).resizable().aspectRatio(contentMode: .fill).background(Color.gray).clipShape(Circle()).frame(width: 30, height: 30, alignment: .center)
-                VStack(alignment: .leading, spacing: 3){
-                    Text(model.userInfo.nickName).font(.system(size: 13)).foregroundColor(.colorWithHexString(hex: "#999999"))
-                    Text(model.comment).font(.system(size: 15)).lineSpacing(5)
-                }
-                Spacer()
-            }
-                HStack(alignment: .center, spacing: 5){
-                    Spacer().frame(width:55)
-                    Text(model.createAt).font(.system(size: 12)).foregroundColor(.colorWithHexString(hex: "#999999"))
-                    Text("回复").font(.system(size: 12, weight: .medium, design: .default)).foregroundColor(.colorWithHexString(hex: "#999999"))
-                }
-        }.background(Color.white).onTapGesture {
+        Button {
             tapModel.sectionTap = true
             tapModel.commentId = model.id
             tapModel.atUid = model.uid
             tapModel.nickName = model.userInfo.nickName
-        }
+        } label: {
+            VStack(alignment: .leading, spacing: 5) {
+                HStack(alignment: .top, spacing: 5){
+                    Spacer().frame(width:20)
+                    WebImage(url: URL(string: model.userInfo.avatar)).resizable().aspectRatio(contentMode: .fill).background(Color.gray).clipShape(Circle()).frame(width: 30, height: 30, alignment: .center)
+                    VStack(alignment: .leading, spacing: 3){
+                        Text(model.userInfo.nickName).font(.system(size: 13)).foregroundColor(.colorWithHexString(hex: "#999999"))
+                        Text(model.comment).font(.system(size: 15)).lineSpacing(5)
+                    }
+                    Spacer()
+                }
+                HStack(alignment: .center, spacing: 5){
+                    Spacer().frame(width:55)
+                    Text(model.createAt).font(.system(size: 12)).foregroundColor(.colorWithHexString(hex: "#999999"))
+                    Text("回复").font(.system(size: 12, weight: .medium, design: .default)).foregroundColor(.colorWithHexString(hex: "#999999"))
+                    Spacer()
+                }
+            }.contentShape(Rectangle()).background(Color.white).id(model.id)
+        }.buttonStyle(PlainButtonStyle()).foregroundColor(.black)
+
+          
+        
     }
     
 }
 
 struct SecondaryCommentRow:View{
     var model : SecondaryCommentModel
+    @EnvironmentObject var tapObserModel : ObserverTapModel
     var body: some View{
-        VStack(alignment: .leading, spacing: 0) {
-            HStack(alignment: .center, spacing: 5){
-                Spacer().frame(width:55)
-                WebImage(url: URL(string: model.uidInfo.avatar)).resizable().aspectRatio(contentMode: .fill).background(Color.gray).clipShape(Circle()).frame(width: 20, height: 20, alignment: .center)
-                Text(model.atUid > 0 ? "\(model.uidInfo.nickName)►\(model.atUidInfo.nickName)" : "\(model.uidInfo.nickName)").font(.system(size: 13)).foregroundColor(.colorWithHexString(hex: "#999999"))
-                Spacer()
-            }
-            Spacer().frame(height:3)
-            HStack(alignment: .center, spacing: 0) {
-                Spacer().frame(width:85)
-                Text(model.comment).font(.system(size: 15)).lineSpacing(5)
-            }
-            Spacer().frame(height:5)
-            HStack(alignment: .center, spacing: 5){
-                Spacer().frame(width:80)
-                Text(model.createAt).font(.system(size: 12)).foregroundColor(.gray)
-                Text("回复").font(.system(size: 12, weight: .medium, design: .default)).foregroundColor(.colorWithHexString(hex: "#999999"))
-            }
-        }
+        Button {
+            tapObserModel.commentId = model.commentId
+            tapObserModel.atUid = model.uid
+            tapObserModel.nickName = model.atUidInfo.nickName
+            tapObserModel.sectionTap = true
+        } label: {
+            VStack(alignment: .leading, spacing: 0) {
+                HStack(alignment: .center, spacing: 5){
+                    Spacer().frame(width:55)
+                    WebImage(url: URL(string: model.uidInfo.avatar)).resizable().aspectRatio(contentMode: .fill).background(Color.gray).clipShape(Circle()).frame(width: 20, height: 20, alignment: .center)
+                    Text(model.atUid > 0 ? "\(model.uidInfo.nickName)►\(model.atUidInfo.nickName)" : "\(model.uidInfo.nickName)").font(.system(size: 13)).foregroundColor(.colorWithHexString(hex: "#999999"))
+                    Spacer()
+                }
+                Spacer().frame(height:3)
+                HStack(alignment: .center, spacing: 0) {
+                    Spacer().frame(width:85)
+                    Text(model.comment).font(.system(size: 15)).lineSpacing(5)
+                    Spacer()
+                }
+                Spacer().frame(height:5)
+                HStack(alignment: .center, spacing: 5){
+                    Spacer().frame(width:80)
+                    Text(model.createAt).font(.system(size: 12)).foregroundColor(.gray)
+                    Text("回复").font(.system(size: 12, weight: .medium, design: .default)).foregroundColor(.colorWithHexString(hex: "#999999"))
+                    Spacer()
+                }
+            }.contentShape(Rectangle())
+        }.buttonStyle(PlainButtonStyle()).foregroundColor(.black)
+
+          
+        
     }
 }
 
